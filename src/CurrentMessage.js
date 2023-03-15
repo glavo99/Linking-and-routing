9239103cf07fb958746168e0d7bdd5d8696c34c3
glavo99 <elgendy.yousef.1999@gmail.com>
2023-03-15 21:51:28 +0200

    added comment file for conditional

---
+++ src/CurrentMessage.js
@@ -0,0 +1,14 @@
+// function CurrentMessage() {
+//       const day = new Date().getDay();
+//       if (day >= 1 && day <= 5) {
+//           return <Workdays />
+//       }
+//       return <Weekends />
+//   }
+
+  // function CurrentMessage(props) {
+  //       if (props.day >= 1 && props.day <= 5) {
+  //           return <Workdays />
+  //       }
+  //       return <Weekends />
+  //   }
