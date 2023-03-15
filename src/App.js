a51cd450df478234896147e678584e4c67f8e3fd
glavo99 <elgendy.yousef.1999@gmail.com>
2023-03-15 19:50:31 +0200

    added navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import Homepage from "./Homepage";
+import AboutMe from "./AboutMe";
+import { Routes, Route , Link} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="nav">
+        <Link to="/" className="nav-item">
+          Homepage
+        </Link>
+        <Link to="/about-me" className="nav-item">
+          About-Me
+        </Link>
+        
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
