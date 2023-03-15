51996e5a6a3958f68795439c2854f267fbd62664
glavo99 <elgendy.yousef.1999@gmail.com>
2023-03-15 22:25:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import Homepage from "./Homepage";
-import AboutMe from "./AboutMe";
-import { Routes, Route , Link} from "react-router-dom";
-import './App.css';
-
 function App() {
+  const time = new Date();
+  const day = time.toLocaleString("en-us", { weekday: "long" });
+  const morning = time.getHours() >= 6 && time.getHours() <= 12;
+  let dayMessage;
+
+  if (day.toLowerCase() === "monday") {
+    dayMessage = `Happy ${day}`;
+  } else if (day.toLowerCase() === "tuesday") {
+    dayMessage = `${day}, 4 days to go`;
+  } else if (day.toLowerCase() === "wednesday") {
+    dayMessage = `${day}, half way there`;
+  } else if (day.toLowerCase() === "thursday") {
+    dayMessage = `${day}, start planning the weekend`;
+  } else if (day.toLowerCase() === "friday") {
+    dayMessage = `Woo - hoo, the weekend is coming!`;
+  } else {
+    dayMessage = `stay calm and keep having fun`;
+  }
+
   return (
     <div className="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">
-          Homepage
-        </Link>
-        <Link to="/about-me" className="nav-item">
-          About-Me
-        </Link>
-        
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-me" element={<AboutMe />} />
-      </Routes>
+      <h1>
+        {dayMessage}
+      </h1>
+      {morning ? <h2>Have you had breakfast yet?</h2> : ''}
     </div>
   );
 }
