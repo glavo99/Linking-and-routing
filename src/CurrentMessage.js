4a744e5d291d38ee41d5a7f4ff2a3faa8bf20735
glavo99 <elgendy.yousef.1999@gmail.com>
2023-03-15 22:27:32 +0200

    minor changes

--- src/CurrentMessage.js
+++ src/CurrentMessage.js
@@ -1,11 +1,3 @@
-// function CurrentMessage() {
-//       const day = new Date().getDay();
-//       if (day >= 1 && day <= 5) {
-//           return <Workdays />
-//       }
-//       return <Weekends />
-//   }
-
   // function CurrentMessage(props) {
   //       if (props.day >= 1 && props.day <= 5) {
   //           return <Workdays />
